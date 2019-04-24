77e75be8e58fe8a209026a988489d2043f43b676
John Datserakis <johndatserakis@gmail.com>
2019-04-23 22:55:58 -0400

    Rebuilding example

--- example/App.vue
+++ example/App.vue
@@ -18,8 +18,6 @@
                     </template>
 
                 </vue-navigation-bar>
-
-                <vue-navigation-bar :options="navbarOptions1" />
             </div>
         </section>
 
@@ -90,92 +88,6 @@
                         {
                             type: 'link',
                             text: 'Why Dunder Mifflin',
-                            iconLeft: '<i class="fa fa-star fa-fw"></i>',
-                            subMenuOptions: [
-                                {
-                                    type: 'link',
-                                    text: 'About',
-                                    subText: 'Stupid corporate wet blankets. Like booze ever killed anyone.',
-                                    path: './about',
-                                    iconLeft: '<svg id="i-info" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"> <path d="M16 14 L16 23 M16 8 L16 10" /> <circle cx="16" cy="16" r="14" /> </svg>',
-                                },
-                                {
-                                    type: 'hr',
-                                },
-                                {
-                                    type: 'link',
-                                    text: 'Locations',
-                                    subText: 'You\'re a presentation tool!',
-                                    path: './locations',
-                                },
-                                {
-                                    type: 'hr',
-                                },
-                                {
-                                    type: 'link',
-                                    text: 'Blog',
-                                    subText: 'I enjoy having breakfast in bed. I like waking up to the smell of bacon. Sue me.',
-                                    path: './blog',
-                                },
-                            ]
-                        },
-                        {
-                            type: 'link',
-                            text: 'Contact',
-                            subMenuOptions: [
-                                {
-                                    type: 'link',
-                                    text: 'Customer Service',
-                                    path: './customer-service'
-                                },
-                                {
-                                    type: 'link',
-                                    text: 'Accounting',
-                                    path: './accounting',
-                                },
-                                {
-                                    type: 'hr',
-                                },
-                                {
-                                    type: 'link',
-                                    text: 'Reception',
-                                    path: './reception',
-                                    iconLeft: '<svg id="i-telephone" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"> <path d="M3 12 C3 5 10 5 16 5 22 5 29 5 29 12 29 20 22 11 22 11 L10 11 C10 11 3 20 3 12 Z M11 14 C11 14 6 19 6 28 L26 28 C26 19 21 14 21 14 L11 14 Z" /> <circle cx="16" cy="21" r="4" /> </svg>',
-                                },
-                            ]
-                        }
-                    ],
-                    menuOptionsRight: [
-                        {
-                            type: 'button',
-                            text: 'Signup',
-                            path: './signup',
-                            class: 'button-red',
-                            iconLeft: '<svg id="i-star" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="currentcolor" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"> <path d="M16 2 L20 12 30 12 22 19 25 30 16 23 7 30 10 19 2 12 12 12 Z" /> </svg>'
-                        },
-                        {
-                            type: 'button',
-                            text: 'Login',
-                            path: './login',
-                            iconRight: '<svg id="i-arrow-right" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"> <path d="M22 6 L30 16 22 26 M30 16 L2 16" /> </svg>'
-                        }
-                    ]
-                },
-                navbarOptions1: {
-                    elementId: 'main-navbar1',
-                    isUsingVueRouter: true,
-                    mobileBreakpoint: 992,
-                    brandImagePath: './',
-                    brandImage: require('../src/assets/images/lockup-color.png'),
-                    brandImageAltText: 'brand-image',
-                    collapseButtonImageOpen: require('../src/assets/images/collapse-menu-dark.png'),
-                    collapseButtonImageClose: require('../src/assets/images/times.png'),
-                    showBrandImageInMobilePopup: true,
-                    ariaLabelMainNav: 'Main Navigation',
-                    menuOptionsLeft: [
-                        {
-                            type: 'link',
-                            text: '1Why Dunder Mifflin',
                             iconLeft: '<i class="fa fa-star fa-fw"></i>',
                             subMenuOptions: [
                                 {
